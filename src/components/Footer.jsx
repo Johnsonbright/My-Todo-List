5ee41a313cf2345c47b417308272536c614731d7
Uzoma Ifechukwu <johnsonbright101@gmail.com>
2024-06-22 13:46:39 -0700

    Add New Feature

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from "./footer.module.css"
 
 export default function Footer({completedTodos, totalTodos}) {
+  console.log("My First React Project")
   return (
     <div className={styles.footer}>
       <span className={styles.item}>Completed Todos: {completedTodos}</span>
